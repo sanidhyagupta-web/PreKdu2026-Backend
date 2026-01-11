af4ac38ea8dd0a2a7c56929d5a248d44e4d8f7c8
sanidhyagupta-web <sanidhya.gupta@kickdrumtech.com>
2026-01-11 22:10:18 +0530

    Delete Downloads/JWT_Learn/src directory

--- Downloads/JWT_Learn/src/main/java/com/example/demo/Config/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Config;
-
-public class AppConstants {
-    public final static String key = "afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf";
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
-}
